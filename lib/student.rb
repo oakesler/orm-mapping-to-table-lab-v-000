652b57f6e309a38a620c51cf60ec8652172ff039
Olivia Kesler <oakesler@protonmail.ch>
2019-12-11 06:14:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -25,7 +25,7 @@ class Student
     student = Student.new(:name, :grade)
     student.save
     student
-    DB[:conn].execute(sql, name, grade)
+    DB[:conn].execute(sql, :name, :grade)
   end
 
 def save
