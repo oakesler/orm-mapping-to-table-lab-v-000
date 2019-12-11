0befb02c6e7634a04015106b6fafc0d54544b000
Olivia Kesler <oakesler@protonmail.ch>
2019-12-11 06:15:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -33,7 +33,7 @@ def save
     INSERT INTO students(name, grade)
     VALUES (?, ?)
     SQL
-    DB[:conn].execute(sql, name, grade)
+    DB[:conn].execute(sql, @name, @grade)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]  
   end
   
